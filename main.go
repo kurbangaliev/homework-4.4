830042255c69628fb5cb3baefcb6f2e0a57e8302
d.kurbangaliev <d.kurbangaliev@darrail.com>
2025-11-06 21:43:39 +0500

    Initial commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func sumAll(numbers ...int) int {
+	sum := 0
+	for _, n := range numbers {
+		sum += n
+	}
+	return sum
+}
+
+func main() {
+	fmt.Println(sumAll(1, 2, 3))
+	fmt.Println(sumAll(10, -2, 4, 7))
+}
